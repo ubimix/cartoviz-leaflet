8dd5e6d198cf53b0ed17de573bd48916f418dc05
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-04-16 22:41:12 +0200

    Modifications for WeViz

--- slide_config.js
+++ slide_config.js
@@ -1,7 +1,7 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Atelier Cartoviz',
+    title: 'Atelier WeViz',
     subtitle: 'Présentation de Leaflet',
     //eventInfo: {
     //  title: 'Google I/O',
@@ -12,7 +12,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/favicon.ico',
+    // favIcon: 'images/favicon.ico',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
